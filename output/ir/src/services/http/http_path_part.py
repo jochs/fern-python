057ada1f712783084fce5dd7011d179986257961
Zach Kirsch <zachkirsch@gmail.com>
2022-09-19 17:46:28 -0700

    Remove output/

--- output/ir/src/services/http/http_path_part.py
+++
@@ -1,9 +0,0 @@
-import pydantic
-
-
-class HttpPathPart(pydantic.BaseModel):
-    path_parameter: int = pydantic.Field(alias="pathParameter")
-    tail: int
-
-    class Config:
-        allow_population_by_field_name = True
