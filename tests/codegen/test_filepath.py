edda492f7cba2afe4678a073f7e6eb08437e4b4c
dsinghvi <deep@usebirch.com>
2022-10-06 01:45:50 -0700

    fix: filename logged should be a string

---
+++ tests/codegen/test_filepath.py
@@ -0,0 +1,6 @@
+from fern_python.codegen.filepath import Filepath
+
+
+def test_filepath_to_str(): 
+    filepath = Filepath(directories=[Filepath.DirectoryFilepathPart(module_name="dir1"), Filepath.DirectoryFilepathPart(module_name="dir2")], file=Filepath.FilepathPart(module_name="file"))
+    assert filepath.to_str() == "dir1/dir2/file.py"
