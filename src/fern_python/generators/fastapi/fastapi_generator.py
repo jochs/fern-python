28e687d75ae3f8d3aca3a1acd510254a3ed1dd4f
Zach Kirsch <zachkirsch@gmail.com>
2022-10-06 19:11:32 -0700

    Start FastAPI generator

---
+++ src/fern_python/generators/fastapi/fastapi_generator.py
@@ -0,0 +1,16 @@
+from generator_exec.resources.config import GeneratorConfig
+
+from fern_python.cli.abstract_generator import AbstractGenerator
+from fern_python.generated import ir_types
+from fern_python.generator_exec_wrapper import GeneratorExecWrapper
+
+
+class FastApiGenerator(AbstractGenerator):
+    def run(
+        self,
+        *,
+        generator_exec_wrapper: GeneratorExecWrapper,
+        ir: ir_types.IntermediateRepresentation,
+        generator_config: GeneratorConfig,
+    ) -> None:
+        ...
--- src/fern_python/generators/pydantic_model/pydantic_model_generator.py
+++ src/fern_python/generators/pydantic_model/pydantic_model_generator.py
@@ -1,26 +1,18 @@
 from generator_exec.resources.config import GeneratorConfig
 from generator_exec.resources.logging import GeneratorUpdate, LogLevel, LogUpdate
 
-from ...cli.abstract_generator import AbstractGenerator
-from ...codegen import Project
-from ...generated import ir_types
-from ...generator_exec_wrapper import GeneratorExecWrapper
-from ...logger import Logger
+from fern_python.cli.abstract_generator import AbstractGenerator
+from fern_python.codegen import Project
+from fern_python.generated import ir_types
+from fern_python.generator_exec_wrapper import GeneratorExecWrapper
+
 from .context import DeclarationHandlerContextImpl
 from .custom_config import CustomConfig
 from .type_declaration_handler import TypeDeclarationHandler
 from .type_declaration_referencer import TypeDeclarationReferencer
 
 
-class LoggerImpl(Logger):
-    def log(self, content: str) -> None:
-        print(content)
-
-
 class PydanticModelGenerator(AbstractGenerator):
-    def __init__(self) -> None:
-        self._logger = LoggerImpl()
-
     def run(
         self,
         *,
@@ -33,7 +25,7 @@ class PydanticModelGenerator(AbstractGenerator):
             type_declaration_referencer = TypeDeclarationReferencer(api_name=ir.api_name)
             for type_to_generate in ir.types:
                 self._generate_type(
-                    project,
+                    project=project,
                     ir=ir,
                     type=type_to_generate,
                     generator_exec_wrapper=generator_exec_wrapper,
@@ -61,7 +53,7 @@ class PydanticModelGenerator(AbstractGenerator):
                 type_declaration_referencer=type_declaration_referencer,
             )
             type_declaration_handler = TypeDeclarationHandler(
-                declaration=type, context=context, logger=self._logger, custom_config=custom_config
+                declaration=type, context=context, custom_config=custom_config
             )
             type_declaration_handler.run()
             generator_exec_wrapper.send_update(
