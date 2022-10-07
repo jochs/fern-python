a94a52c5edead7ca4bb8bf0329fac9485aea862f
Zach Kirsch <zachkirsch@gmail.com>
2022-10-07 13:58:31 -0700

    Don't generate src/ for local files

--- tests/fastapi/snapshots/snap_test_fastapi/test_fastapi src_my-api_lang_server___init__.py
+++
@@ -1,3 +0,0 @@
-from .types import LangServerRequest, LangServerResponse
-
-__all__ = ["LangServerRequest", "LangServerResponse"]
--- tests/pydantic_model/snapshots/snap_test_pydantic_model/test_pydantic_model src_fern_playlist___init__.py
+++
@@ -1,6 +0,0 @@
-from .playlist import Playlist
-from .playlist_create_request import PlaylistCreateRequest
-from .playlist_id import PlaylistId
-from .update_playlist_request import UpdatePlaylistRequest
-
-__all__ = ["Playlist", "PlaylistCreateRequest", "PlaylistId", "UpdatePlaylistRequest"]
