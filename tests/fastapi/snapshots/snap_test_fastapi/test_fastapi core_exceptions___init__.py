39f58ce75ae6c38dbb8c28bb015ce0d8bdadae54
Zach Kirsch <zachkirsch@gmail.com>
2022-10-12 13:53:04 -0700

    Fix snapshots

--- tests/fastapi/snapshots/snap_test_fastapi/test_fastapi core_exceptions___init__.py
+++ tests/fastapi/snapshots/snap_test_fastapi/test_fastapi core_exceptions___init__.py
@@ -1,8 +1,12 @@
 from __future__ import annotations
 
 from .fern_http_exception import FernHTTPException
+from .handlers import (
+    default_exception_handler,
+    fern_http_exception_handler,
+    http_exception_handler,
+)
 from .unauthorized import UnauthorizedException
-from .handlers import fern_http_exception_handler, http_exception_handler, default_exception_handler
 
 __all__ = [
     "UnauthorizedException",
