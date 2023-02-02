021ad4eb71debe099761a437f2707b2799a37e3f
Zach Kirsch <zachkirsch@gmail.com>
2023-02-01 22:21:58 -0800

    Upgrade IR to 0.0.918

--- src/fern_python/generators/fastapi/declaration_referencers/service_declaration_referencer.py
+++ src/fern_python/generators/fastapi/declaration_referencers/service_declaration_referencer.py
@@ -1,6 +1,6 @@
 import fern.ir.pydantic as ir_types
 
-from fern_python.codegen import ExportStrategy, Filepath
+from fern_python.codegen import Filepath
 
 from .fastapi_declaration_referencer import FastApiDeclarationReferencer
 
@@ -12,10 +12,11 @@ class ServiceDeclarationReferencer(FastApiDeclarationReferencer[ir_types.http.De
                 *self._get_directories_for_fern_filepath(
                     fern_filepath=name.fern_filepath,
                 ),
-                Filepath.DirectoryFilepathPart(module_name="service", export_strategy=ExportStrategy.EXPORT_ALL),
+                Filepath.DirectoryFilepathPart(module_name="service"),
             ),
             file=Filepath.FilepathPart(module_name="service"),
         )
 
     def get_class_name(self, *, name: ir_types.DeclaredServiceName) -> str:
-        return f"Abstract{name.name.original_name}"
+        last_part = name.fern_filepath.all_parts[-1]
+        return f"Abstract{last_part.pascal_case.unsafe_name if last_part is not None else 'Root'}Service"
