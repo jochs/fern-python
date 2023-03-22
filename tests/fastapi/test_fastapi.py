706049f54222c1c4db2ab7168d648dae3f32f287
Zach Kirsch <zachkirsch@gmail.com>
2023-03-21 17:39:24 -0700

    Add config to include/exclude union visitors

--- tests/fastapi/test_fastapi.py
+++ tests/fastapi/test_fastapi.py
@@ -8,7 +8,14 @@ from ..snapshot_helpers import run_snapshot_test
 
 
 def test_fastapi(snapshot: SnapshotTest, tmpdir: Path) -> None:
-    run_snapshot_test(snapshot=snapshot, fixture_name="trace", tmpdir=tmpdir, cli=cli, filename_of_test=__file__)
+    run_snapshot_test(
+        snapshot=snapshot,
+        fixture_name="trace",
+        tmpdir=tmpdir,
+        cli=cli,
+        filename_of_test=__file__,
+        custom_config={"include_validators": True, "include_union_visitors": True},
+    )
 
 
 def test_fastapi_error_status_code(snapshot: SnapshotTest, tmpdir: Path) -> None:
