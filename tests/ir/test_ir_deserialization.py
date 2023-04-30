d9d6d39a5d4ffc8232777bcaec22f5521eb4a45c
Zach Kirsch <zachkirsch@gmail.com>
2023-04-30 14:56:10 -0700

    Add test for IR serde failure for examples

---
+++ tests/ir/test_ir_deserialization.py
@@ -0,0 +1,28 @@
+import os
+import subprocess
+
+from fern.ir import IntermediateRepresentation
+
+
+def test_ir_deserialization() -> None:
+    path_to_ir = os.path.join(os.path.dirname(__file__), "fixtures/fern/examples/ir.json")
+    subprocess.run(
+        [
+            "npx",
+            "--yes",
+            "fern-api",
+            "ir",
+            path_to_ir,
+            "--language",
+            "python",
+            "--api",
+            "examples",
+        ],
+        cwd=os.path.join(
+            os.path.dirname(__file__),
+            "fixtures",
+        ),
+        check=True,
+    )
+
+    IntermediateRepresentation.parse_file(path_to_ir)
