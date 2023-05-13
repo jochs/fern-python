12d9bc375d584df5c59ed81882db453a9e4f5068
dsinghvi <deep@usebirch.com>
2023-05-13 14:53:17 -0400

    fix pre-commit

--- src/fern_python/generators/sdk/client_generator/client_generator.py
+++ src/fern_python/generators/sdk/client_generator/client_generator.py
@@ -6,12 +6,7 @@ from typing_extensions import Never
 
 from fern_python.codegen import AST, SourceFile
 from fern_python.codegen.ast.nodes.code_writer.code_writer import CodeWriterFunction
-from fern_python.external_dependencies import (
-    HttpX,
-    Json,
-    Pydantic,
-    UrlLibParse,
-)
+from fern_python.external_dependencies import HttpX, Json, Pydantic, UrlLibParse
 
 from ..context.sdk_generator_context import SdkGeneratorContext
 from ..environment_generators import (
